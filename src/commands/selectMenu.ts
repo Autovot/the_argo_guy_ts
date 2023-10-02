3839b1131efffbe24c8800c4ea5f0af03dccb20a
Autovot <87210193+Autovot@users.noreply.github.com>
2023-10-03 01:26:41 +0200

    Fusioned selectMenu and foro, temporarily

--- src/commands/selectMenu.ts
+++ src/commands/selectMenu.ts
@@ -1,14 +1,36 @@
-import { ActionRowBuilder, CommandInteraction, MessageActionRowComponentBuilder, ChannelSelectMenuBuilder, ChannelType, ChannelSelectMenuInteraction } from 'discord.js'
+import { ActionRowBuilder, CommandInteraction, MessageActionRowComponentBuilder, ChannelSelectMenuBuilder, ChannelType, ChannelSelectMenuInteraction, CacheType, REST } from 'discord.js'
 import { Discord, Slash, SelectMenuComponent } from 'discordx'
 
 @Discord()
 export class SelectMenu {
+  async handlerForum(interaction: ChannelSelectMenuInteraction<CacheType>) {
+    const userPost = interaction.user
+    const TOKEN = process.env.BOT_TOKEN
+    if (TOKEN === undefined) throw Error('Could not find BOT_TOKEN in your environment')
+
+    const rest = new REST({ version: '10' }).setToken(TOKEN)
+    try {
+      await rest.post(`/channels/${interaction.values[0]}/threads`, {
+        body: {
+          name: 'Post mandado por ' + userPost.username,
+          auto_archive_duration: 60,
+          rate_limit_per_user: 0,
+          message: {
+            content: `Hello, ${userPost}!`
+          }
+        }
+      })
+        .then(console.log)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   @SelectMenuComponent({ id: "channel_menu" })
   async selectMenu (interaction: ChannelSelectMenuInteraction ): Promise<void> {
-    interaction.reply({
-      content: `You have selected ${interaction.values}!`,
-      ephemeral: true,
-    })
+    // Need call handlerForum in foro.ts for create post with ForumID
+    this.handlerForum(interaction)
+    await interaction.reply({ content: 'Forum ID set', ephemeral: true })
   }
 
 
